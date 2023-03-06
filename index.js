dfa0b017eb684af71a048fff3d541b1e0a9fbdee
Busolami <oluwabibusola@gmail.com>
2023-03-06 18:17:34 +0100

    final commit

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-//Question 1 is in the READme.md file 
+//Question 1 
+//1a) MUTATING ARRAY METHOD AND NON MUTATING ARRAY METHOD
+
+//i)Mutating array use buit in array method to make change to an array of oject; also the change made, changes the sequence of initial variable declared because it point to the sam refernce.
+
+//ii) Non-mutating array; the method don not cause a change in the original arrays declared.
+
+//1b List five examples each
+//i) Mutating Array Method Example
+// array.push()
+// array.unshift()
+// array.reverse()
+// array.splice()
+// array.sort()
+
+// // ii) Examples of non mutating Array method
+// array.slice()
+// array.concat()
+// array.map()
+// array.filter()
+// array.reduce()
 
 //Question 2
 const languages = ['C#', 'JavaScript', 'Ruby', 'PHP', 'Python'];
@@ -27,45 +47,33 @@ let fruit = ['apple', 'mango', 'banana'];
 
 function changeFruit(fruit) {
     fruit[2] = "orange";
-    console.log(changeFruit);
+    return fruit;
 }
+console.log(changeFruit(fruit));
+
+
+
 
 //Question 4
 //Write a function that accepts an array of numbers as an argument. Return the maximum value in the array.
 
-class maximumInterger {
-    findMaximum(array) {
-        let maximumNumberFinder = [4, 5, 10, -2];
-        const arrayLength = array.length;
-        for (let i = 0; i < arrayLength; i++) {
-            if (i === 10) {
-                maximumNumberFinder = array[i];
-            }
-            if (array[i] > maximumNumberFinder) {
-                maximumNumberFinder = array[i];
-            }
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function findMax(numbers) {
+    let max = numbers[0]
+
+    for (let index = 0; index < numbers.length; index++) {
+        if (numbers[index] > max) {
+            max = numbers[index]
         }
-        console.log(maximumNumberFinder);
+
     }
+    return max;
 }
+let max = findMax(numbers)
+console.log(max);
 //
-// class SmallestIntegerFinder {
-//     findSmallestInt(array) {
 
-//         let smallestnumber;
-//         const loa = array.length;
-//         for (let i = 0; i < loa; i++) {
-//             if (i === 0) {
-//                 smallestnumber = array[i];
-//             }
-//             if (array[i] < smallestnumber) {
-//                 smallestnumber = array[i];
-//             }
-
-//         }
-//         return smallestnumber;
-//     }
-// }
 
 // Question 5
 
